e550514697a696e464b7a5196765ad28523f945d
Kooreung <158035686+Kooreung@users.noreply.github.com>
2024-02-26 17:31:32 +0900

    [level 0] Title: 문자열 곱하기, Time: 1.63 ms, Memory: 77.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181940. 문자열 곱하기/문자열 곱하기.java
@@ -0,0 +1,11 @@
+class Solution {
+    public String solution(String my_string, int k) {
+        String answer = "";
+        String str = "";
+        
+        for (int i = 0; i < k; i++) {
+            answer = answer + my_string;
+        }
+        return answer;
+    }
+}
