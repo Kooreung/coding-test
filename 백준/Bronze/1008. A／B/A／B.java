a4d550772cd477588b164c3e40d6227b25cf8463
Kooreung <158035686+Kooreung@users.noreply.github.com>
2024-02-27 15:49:51 +0900

    [Bronze V] Title: A/B, Time: 208 ms, Memory: 17780 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double a = scanner.nextInt();
+        double b = scanner.nextInt();
+
+        System.out.println(a/b);
+        
+    }
+}
