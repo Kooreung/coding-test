b5e5536c79e3895cf493c8fa5f731238bfaa2aa7
Kooreung <158035686+Kooreung@users.noreply.github.com>
2024-03-14 17:32:32 +0900

    [Bronze V] Title: 개, Time: 112 ms, Memory: 13860 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        String answer =
+                "|\\_/|\n" +
+                "|q p|   /}\n" +
+                "( 0 )\"\"\"\\\n" +
+                "|\"^\"`    |\n" +
+                "||_/=\\\\__|\n";
+
+
+        System.out.println(answer);
+    }
+}
