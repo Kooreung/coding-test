0f534057855057c43d3dc09b09846c250ec92943
Kooreung <158035686+Kooreung@users.noreply.github.com>
2024-03-14 17:30:20 +0900

    [Bronze V] Title: 고양이, Time: 116 ms, Memory: 13880 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        String answer =
+                "\\    /\\\n" +
+                " )  ( ')\n" +
+                "(  /  )\n" +
+                " \\(__)|\n";
+
+
+        System.out.println(answer);
+    }
+}
