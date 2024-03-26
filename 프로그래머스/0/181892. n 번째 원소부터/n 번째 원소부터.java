433a2c9c5611edc7ba3549fe9b966de0073a2265
Kooreung <158035686+Kooreung@users.noreply.github.com>
2024-03-26 11:30:43 +0900

    [level 0] Title: n 번째 원소부터, Time: 0.01 ms, Memory: 73.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181892. n 번째 원소부터/n 번째 원소부터.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] num_list, int n) {
+        int[] answer = {};
+        
+        answer = Arrays.copyOfRange(num_list,n - 1,num_list.length);
+        
+        return answer;
+    }
+}
