eb89acf9ac29fd880245be901f20bab1f2a84da8
Kooreung <158035686+Kooreung@users.noreply.github.com>
2024-03-26 11:33:58 +0900

    [level 0] Title: 더 크게 합치기, Time: 7.70 ms, Memory: 81.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int solution(int a, int b) {
+        int answer = 0;
+        int sum1 = Integer.parseInt(a + "" + b);
+        int sum2 = Integer.parseInt(b + "" + a);
+
+        if (sum1 >= sum2) {
+            answer = sum1;
+        } else {
+            answer = sum2;
+        }
+        
+        return answer;
+    }
+}
