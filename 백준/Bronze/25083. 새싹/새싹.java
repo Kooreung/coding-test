02304a9806204eb48f483e181b471b42d67cbf80
Kooreung <158035686+Kooreung@users.noreply.github.com>
2024-04-15 15:19:51 +0900

    [Bronze V] Title: 새싹, Time: 120 ms, Memory: 13920 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        String line1 = "         ,r'\"7";
+        String line2 = "r`-_   ,'  ,/";
+        String line3 = " \\. \". L_r'";
+        String line4 = "   `~\\/";
+        String line5 = "      |";
+        String line6 = "      |";
+
+        System.out.println(line1);
+        System.out.println(line2);
+        System.out.println(line3);
+        System.out.println(line4);
+        System.out.println(line5);
+        System.out.println(line6);
+    }
+}
