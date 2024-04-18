cb7ca2d159e6cd0ce31f9f5b52cab6544b8f58c5
Kooreung <158035686+Kooreung@users.noreply.github.com>
2024-04-18 21:38:49 +0900

    [Bronze V] Title: 사파리월드, Time: 140 ms, Memory: 14604 KB -BaekjoonHub

---
+++ 백준/Bronze/2420. 사파리월드/사파리월드.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        long N = Long.parseLong(st.nextToken());
+        long M = Long.parseLong(st.nextToken());
+
+        bw.write(Math.abs(N - M) + "\n");
+
+        bw.flush();
+        br.close();
+        bw.close();
+    }
+}
