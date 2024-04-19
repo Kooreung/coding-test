5e23e1db70e6297429e0034d11df524302f75a6d
Kooreung <158035686+Kooreung@users.noreply.github.com>
2024-04-19 17:39:15 +0900

    [Bronze V] Title: 검증수, Time: 124 ms, Memory: 14160 KB -BaekjoonHub

--- 백준/Bronze/2475. 검증수/검증수.java
+++ 백준/Bronze/2475. 검증수/검증수.java
@@ -4,23 +4,16 @@ import java.util.StringTokenizer;
 public class Main {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st = new StringTokenizer(br.readLine());
         int[] numArr = new int[5];
         int sum = 0;
 
         for (int i = 0; i <= 4; i++) {
             numArr[i] = Integer.parseInt(st.nextToken());
-        }
-
-        for (int i = 0; i < 5; i++) {
             sum += numArr[i] * numArr[i];
         }
 
-        bw.write((sum % 10) + "\n");
-
+        System.out.println(sum % 10);
         br.close();
-        bw.flush();
-        bw.close();
     }
 }
