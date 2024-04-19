a1f67d9f7e7189d211ed6103c05937133a17edac
Kooreung <158035686+Kooreung@users.noreply.github.com>
2024-04-19 17:25:54 +0900

    [Bronze V] Title: 이상한 기호, Time: 136 ms, Memory: 14452 KB, Score: 30 point -BaekjoonHub

---
+++ 백준/Bronze/15964. 이상한 기호/이상한 기호.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+
+        bw.write((A + B) * (A - B) + "\n");
+
+        br.close();
+        bw.flush();
+        bw.close();
+    }
+}
