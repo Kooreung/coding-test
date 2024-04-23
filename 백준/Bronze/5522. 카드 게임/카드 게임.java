81ec2d2906b07cd87b07274922e8f7101430c01d
Kooreung <158035686+Kooreung@users.noreply.github.com>
2024-04-23 17:48:29 +0900

    [Bronze V] Title: 카드 게임, Time: 124 ms, Memory: 14084 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/5522. 카드 게임/카드 게임.java
@@ -0,0 +1,15 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int a = Integer.parseInt(br.readLine());
+        int b = Integer.parseInt(br.readLine());
+        int c = Integer.parseInt(br.readLine());
+        int d = Integer.parseInt(br.readLine());
+        int e = Integer.parseInt(br.readLine());
+
+        int sum = a + b + c + d + e;
+        System.out.println(sum);
+    }
+}
