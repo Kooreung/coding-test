e8923d0f33e645e4dcb79eb98f5077c446bc1c40
Kooreung <158035686+Kooreung@users.noreply.github.com>
2024-04-25 17:26:30 +0900

    [Bronze V] Title: 조별과제를 하려는데 조장이 사라졌다, Time: 216 ms, Memory: 17708 KB -BaekjoonHub

--- 백준/Bronze/15727. 조별과제를 하려는데 조장이 사라졌다/조별과제를 하려는데 조장이 사라졌다.java
+++ 백준/Bronze/15727. 조별과제를 하려는데 조장이 사라졌다/조별과제를 하려는데 조장이 사라졌다.java
@@ -6,31 +6,10 @@ public class Main {
         int t = sc.nextInt();
         int min = 0;
 
-        while (t > 0) {
-            if (t >= 5) {
-                min += t / 5;
-                t = t % 5;
-            }
-            if (t == 4) {
-                min += t / 4;
-                t = t % 4;
-            }
-            if (t == 3) {
-                min += t / 3;
-                t = t % 3;
-            }
-            if (t == 2) {
-                min += t / 2;
-                t = t % 2;
-            }
-            if (t == 1) {
-                min += t / 1;
-                t = t % 1;
-            }
-            if (t == 0) {
-                break;
-            }
+        if (t % 5 == 0) {
+            System.out.println(t / 5);
+        } else {
+            System.out.println((t / 5) + 1);
         }
-        System.out.println(min);
     }
 }
