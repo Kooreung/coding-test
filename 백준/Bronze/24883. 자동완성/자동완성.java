05942f49d93ce1822fc67cc6c589bd883dd7b336
Kooreung <158035686+Kooreung@users.noreply.github.com>
2024-04-27 00:56:48 +0900

    [Bronze V] Title: 자동완성, Time: 124 ms, Memory: 14220 KB -BaekjoonHub

---
+++ 백준/Bronze/24883. 자동완성/자동완성.java
@@ -0,0 +1,16 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+        if (str.equals("N") || str.equals("n")) {
+            System.out.println("Naver D2");
+        } else {
+            System.out.println("Naver Whale");
+        }
+
+        br.close();
+    }
+}
