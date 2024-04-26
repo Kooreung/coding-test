c55ecfda1aba5e2e3c0c845218abaecd2e644205
Kooreung <158035686+Kooreung@users.noreply.github.com>
2024-04-27 00:38:51 +0900

    [Bronze V] Title: 달달함이 넘쳐흘러, Time: 160 ms, Memory: 16280 KB -BaekjoonHub

---
+++ 백준/Bronze/17256. 달달함이 넘쳐흘러/달달함이 넘쳐흘러.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int ax = Integer.parseInt(st.nextToken());
+        int ay = Integer.parseInt(st.nextToken());
+        int az = Integer.parseInt(st.nextToken());
+        st = new StringTokenizer(br.readLine());
+        int cx = Integer.parseInt(st.nextToken());
+        int cy = Integer.parseInt(st.nextToken());
+        int cz = Integer.parseInt(st.nextToken());
+
+        System.out.println((cx - az) + " " + (cy / ay) + " " + (cz - ax));
+
+        br.close();
+    }
+}
