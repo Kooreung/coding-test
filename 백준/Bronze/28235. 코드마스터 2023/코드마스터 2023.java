46d1e83e6e3e17ed59454d420c5aef4862fe8b88
Kooreung <158035686+Kooreung@users.noreply.github.com>
2024-04-28 15:21:11 +0900

    [Bronze V] Title: 코드마스터 2023, Time: 116 ms, Memory: 14092 KB -BaekjoonHub

---
+++ 백준/Bronze/28235. 코드마스터 2023/코드마스터 2023.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+        
+        switch (str) {
+            case "SONGDO":
+                System.out.println("HIGHSCHOOL");
+                break;
+            case "CODE":
+                System.out.println("MASTER");
+                break;
+            case "2023":
+                System.out.println("0611");
+                break;
+            case "ALGORITHM":
+                System.out.println("CONTEST");
+                break;
+        }
+    }
+}
