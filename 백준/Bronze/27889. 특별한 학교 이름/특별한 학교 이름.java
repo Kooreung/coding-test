404eb683f5022f5800bab2c5e3b53d80e2ce1ee6
Kooreung <158035686+Kooreung@users.noreply.github.com>
2024-04-28 15:06:29 +0900

    [Bronze V] Title: 특별한 학교 이름, Time: 124 ms, Memory: 14144 KB -BaekjoonHub

---
+++ 백준/Bronze/27889. 특별한 학교 이름/특별한 학교 이름.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+
+        switch (str) {
+            case "NLCS":
+                System.out.println("North London Collegiate School");
+                break;
+            case "BHA":
+                System.out.println("Branksome Hall Asia");
+                break;
+            case "KIS":
+                System.out.println("Korea International School");
+                break;
+            case "SJA":
+                System.out.println("St. Johnsbury Academy");
+                break;
+        }
+        br.close();
+    }
+}
