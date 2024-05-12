f254e440f49dbeff22fd121fbd82ff448e0b0575
Kooreung <158035686+Kooreung@users.noreply.github.com>
2024-05-12 11:19:52 +0900

    [Bronze IV] Title: 입실 관리, Time: 132 ms, Memory: 14272 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/5524. 입실 관리/입실 관리.java
@@ -0,0 +1,18 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int N = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < N; i++) {
+            String M = br.readLine();
+            bw.write(M.toLowerCase() + "\n");
+        }
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
