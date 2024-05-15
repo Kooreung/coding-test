bfe25759d3e0de130e9d6df87cbe01e41b2b1926
Kooreung <158035686+Kooreung@users.noreply.github.com>
2024-05-15 22:33:43 +0900

    [Bronze IV] Title: 심부름 가는 길, Time: 124 ms, Memory: 14144 KB -BaekjoonHub

---
+++ 백준/Bronze/5554. 심부름 가는 길/심부름 가는 길.java
@@ -0,0 +1,18 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int A = Integer.parseInt(br.readLine());
+        int B = Integer.parseInt(br.readLine());
+        int C = Integer.parseInt(br.readLine());
+        int D = Integer.parseInt(br.readLine());
+
+        int sum = A + B + C + D;
+
+        System.out.println(sum / 60);
+        System.out.println(sum % 60);
+        br.close();
+    }
+}
