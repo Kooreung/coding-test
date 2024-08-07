153d154fbdf604c9f90453f26ab7c9a43f6d11ce
KooReung <158035686+Kooreung@users.noreply.github.com>
2024-08-07 15:17:59 +0900

    [level 2] Title: 올바른 괄호, Time: 17.35 ms, Memory: 53 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -1,21 +1,20 @@
+import java.util.*;
+
 class Solution {
     boolean solution(String s) {
-        int openCount = 0;
-        int closeCount = 0;
-
-        for (int i = 0; i < s.length(); i++) {
-            if (s.charAt(i) == '(') {
-                openCount++;
-            } else if (s.charAt(i) == ')') {
-                closeCount++;
-            }
-            if (openCount < closeCount) {
-                return false;
+        Stack<Character> st = new Stack<>();
+        
+        for(char item : s.toCharArray()) {
+            if(item == '(') {
+                st.push('(');
+            } else {
+                if(!st.isEmpty()) {
+                    st.pop();
+                } else {
+                    return false;
+                }
             }
         }
-        if (openCount == closeCount) {
-            return true;
-        }
-        return false;
+        return st.isEmpty() ? true : false;
     }
 }
